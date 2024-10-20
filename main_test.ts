b2ea28c63ec40d37e2af4d5251ea57bf2972642d
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-20 22:21:39 +0500

    feat: multiple functionality completed

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,13 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { add, multiple } from "./main.ts";
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
+});
+
+Deno.test(() => {
+  const got = multiple(2, 3);
+  const expected = 6;
+
+  assertEquals(got, expected)
 });
